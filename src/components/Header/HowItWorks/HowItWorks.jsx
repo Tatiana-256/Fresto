3249f47b70fe5fa273cf612fcaa9f2579dad2a07
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2019-12-03 22:37:28 +0200

    Login page was added.

---
+++ src/components/Header/HowItWorks/HowItWorks.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./HowItWorks.module.css";
+
+class Home extends React.Component {
+  render(props) {
+    return <div></div>;
+  }
+}
+
+export default Home;
