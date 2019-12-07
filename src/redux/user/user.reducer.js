df576ac238d3eda5e358d423e595e85833388e19
Yura Pion <yurapion@gmail.com>
2019-12-07 15:04:10 +0200

    Add redux configuration to the app, also created user reducer and store user in redux

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,20 @@
+import { UsersActionTypes } from "./user.types";
+
+const INITIAL_STATE = {
+  currentUser: null
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UsersActionTypes.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
