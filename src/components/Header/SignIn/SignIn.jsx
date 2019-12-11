1cae74651f037d4e25e500d28b64d8d8c738c74b
Yura Pion <yurapion@gmail.com>
2019-12-11 19:11:05 +0200

    Setting up firebase, add sign in with google feature

--- src/components/Header/SignIn/SignIn.jsx
+++ src/components/Header/SignIn/SignIn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./SignIn.module.css";
+import { auth, signInWithGoogle } from "../../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -13,12 +14,14 @@ class SignIn extends React.Component {
   handleSubmit = async event => {
     event.preventDefault();
 
-    // try {
-    //   await auth.singInWithEmail(email, password);
-    //   this.setState({ email: "", password: "" });
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handelChange = event => {
@@ -31,30 +34,37 @@ class SignIn extends React.Component {
       <div>
         <div className={classes.input}>Sign in</div>
         <div className={classes.form}>
-          <div>
-            <input
-              className={classes.inputForm}
-              type="email"
-              name="email"
-              required
-              placeholder="User name"
-              onChange={this.handelChange}
-              value={this.state.email}
-            />
-            <input
-              className={classes.inputForm}
-              type="password"
-              name="password"
-              placeholder="User password"
-              onChange={this.handelChange}
-              value={this.state.password}
-              required
-              minLength="6"
-            />
-          </div>
-          <div>
-            <button className={classes.btn}>Sign in</button>
-          </div>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <input
+                className={classes.inputForm}
+                type="email"
+                name="email"
+                required
+                placeholder="User name"
+                onChange={this.handelChange}
+                value={this.state.email}
+              />
+              <input
+                className={classes.inputForm}
+                type="password"
+                name="password"
+                placeholder="User password"
+                onChange={this.handelChange}
+                value={this.state.password}
+                required
+                minLength="6"
+              />
+            </div>
+            <div>
+              <button type="submit" className={classes.btn}>
+                Sign in
+              </button>
+              <button onClick={signInWithGoogle} className={classes.btn}>
+                Sign in with Google
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     );
